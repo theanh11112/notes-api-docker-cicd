c18139e845f7d222ccbf9d657d4aa8571c41bdfa
luutheanh075-crypto <luutheanh074@gmail.com>
2026-09-01 10:02:37 +0700

    test fail

--- src/test/java/com/practice/notesapi/NotesApiApplicationTests.java
+++ src/test/java/com/practice/notesapi/NotesApiApplicationTests.java
@@ -2,6 +2,7 @@ package com.practice.notesapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class NotesApiApplicationTests {
@@ -9,5 +10,9 @@ class NotesApiApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	@Test
+    void thisShouldFail() {
+         assertEquals(1, 2);
+    }
 
 }
