b9e566ab0b5d55ca8eeb2ccfc99890dea3d467fa
luutheanh075-crypto <luutheanh074@gmail.com>
2026-09-01 10:05:37 +0700

    revert agian

--- src/test/java/com/practice/notesapi/NotesApiApplicationTests.java
+++ src/test/java/com/practice/notesapi/NotesApiApplicationTests.java
@@ -2,7 +2,7 @@ package com.practice.notesapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class NotesApiApplicationTests {
